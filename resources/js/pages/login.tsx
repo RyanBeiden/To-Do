2e8814ad896df23906ce6fe1e82c04b31a522e7e
Ryan Beiden <rdbeiden@gmail.com>
2025-04-02 00:20:04 -0500

    Render error messages and styling depending on response

--- resources/js/pages/login.tsx
+++ resources/js/pages/login.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Logo from '../components/logo';
 
 export default function Login() {
-  const { data, setData, post, processing } = useForm({
+  const { data, setData, post, processing, errors, clearErrors } = useForm({
     email: '',
     password: '',
   });
@@ -18,27 +18,50 @@ export default function Login() {
     <>
       <Head title="Login" />
       <div className="flex justify-center items-center h-screen">
-        <form className="p-10 border border-gray rounded-lg" onSubmit={submit}>
+        <form
+          className="p-10 border border-gray rounded-lg w-sm"
+          onSubmit={submit}
+        >
           <Logo />
-          <div className="mb-3">
+          <div className="mb-6 relative">
             <label htmlFor="email">Email</label>
             <input
               id="email"
-              className="w-full p-2"
-              type="email"
+              className={
+                errors.email
+                  ? 'w-full p-2 border-red-500'
+                  : 'w-full p-2 border-gray'
+              }
+              type="text"
               value={data.email}
               onChange={(event) => setData('email', event.target.value)}
+              onFocus={() => clearErrors('email')}
             />
+            {errors.email && (
+              <div className="absolute text-xs pt-0.5 text-red-500">
+                {errors.email}
+              </div>
+            )}
           </div>
-          <div className="mb-5">
+          <div className="mb-8 relative">
             <label htmlFor="password">Password</label>
             <input
               id="password"
-              className="w-full p-2"
+              className={
+                errors.password
+                  ? 'w-full p-2 border-red-500'
+                  : 'w-full p-2 border-gray'
+              }
               type="password"
               value={data.password}
               onChange={(event) => setData('password', event.target.value)}
+              onFocus={() => clearErrors('password')}
             />
+            {errors.password && (
+              <div className="absolute text-xs pt-0.5 text-red-500">
+                {errors.password}
+              </div>
+            )}
           </div>
           <button type="submit" className="py-2 w-full" disabled={processing}>
             Login
