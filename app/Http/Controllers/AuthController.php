3ee0cf759b8cf7f8a6dbac2b693ddf741ee7c8df
Ryan Beiden <rdbeiden@gmail.com>
2025-04-01 21:09:14 -0500

    Create store method and route for login submission

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -13,5 +15,16 @@ class AuthController extends Controller
     public function show(): Response
     {
         return Inertia::render('login');
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // @TODO: Log user in & change redirect
+        return to_route('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'show'])->name('welcome');
+Route::post('/login', [AuthController::class, 'store'])->name('login');
