a74741243338b69b4d8ac932265b0e5d7ba67b46
Ryan Beiden <rdbeiden@gmail.com>
2025-04-02 00:16:41 -0500

    Handle authentication upon form submission and redirects

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'show'])->name('welcome');
-Route::post('/login', [AuthController::class, 'store'])->name('login');
+Route::get('/', [LoginController::class, 'show'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+
+Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
