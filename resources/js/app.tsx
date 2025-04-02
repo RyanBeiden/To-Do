2fa338b99160740730127311e2c2629a2ed6bee5
Ryan Beiden <rdbeiden@gmail.com>
2025-04-01 20:43:56 -0500

    Format new components

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,19 +1,19 @@
 import '../css/app.css';
 
 import React from 'react';
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'todoList';
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: name => {
+  resolve: (name) => {
     const pages = import.meta.glob('./pages/**/*.tsx', { eager: true });
 
-    return pages[`./pages/${name}.tsx`]
+    return pages[`./pages/${name}.tsx`];
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    createRoot(el).render(<App {...props} />);
   },
 });
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import Logo from '../components/logo';
 
 export default function Welcome() {
-  const [values, setValues] = useState({ // @TODO: Add a type
+  const [values, setValues] = useState({
+    // @TODO: Add a type
     email: '',
     password: '',
   });
@@ -11,42 +12,39 @@ export default function Welcome() {
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { id, value } = event.target;
 
-    setValues(values => ({
+    setValues((values) => ({
       ...values,
       [id]: value,
     }));
-  };
+  }
 
   return (
     <>
       <Head title="Sign In" />
-      <div className='flex justify-center items-center h-screen'>
-        <form className='p-10 border border-gray rounded-lg'>
+      <div className="flex justify-center items-center h-screen">
+        <form className="p-10 border border-gray rounded-lg">
           <Logo />
-          <div className='mb-3'>
+          <div className="mb-3">
             <label htmlFor="email">Email</label>
             <input
               id="email"
-              className='w-full p-2'
-              type='email'
+              className="w-full p-2"
+              type="email"
               value={values.email}
               onChange={handleChange}
             />
           </div>
-          <div className='mb-5'>
+          <div className="mb-5">
             <label htmlFor="password">Password</label>
             <input
               id="password"
-              className='w-full p-2'
+              className="w-full p-2"
               type="password"
               value={values.password}
               onChange={handleChange}
             />
           </div>
-          <button
-            type="submit"
-            className='py-2 w-full'
-          >
+          <button type="submit" className="py-2 w-full">
             Sign In
           </button>
         </form>
